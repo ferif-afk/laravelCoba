eb6cb875bbfa1897b7f33fcbaf68eb3f52d23660
feri ferdiansyah <feri.syah02@gmail.com>
2020-02-09 16:44:49 +0700

    Create 2020_02_09_094056_create_pasien_table.php

---
+++ database/migrations/2020_02_09_094056_create_pasien_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePasienTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pasien', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nama_pasein');
+            $table->string('ruang');
+            $table->string('jenis_kelamin');
+            $table->string('umur');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pasien');
+    }
+}
