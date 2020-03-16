755b0d3b9ec5936544cc2ec817efc2a08e5400e7
feri ferdiansyah <feri.syah02@gmail.com>
2020-03-16 16:26:51 +0700

    Update laraveldb.sql

--- laraveldb.sql
+++ laraveldb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 02 Feb 2020 pada 16.49
+-- Waktu pembuatan: 16 Mar 2020 pada 10.24
 -- Versi server: 10.4.11-MariaDB
 -- Versi PHP: 7.2.26
 
@@ -25,30 +25,156 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `data_gudang`
+--
+
+CREATE TABLE `data_gudang` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama_obat` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jenis_obat` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `harga` char(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `stok` char(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `gambar` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `data_gudang`
+--
+
+INSERT INTO `data_gudang` (`id`, `nama_obat`, `jenis_obat`, `harga`, `stok`, `gambar`, `created_at`, `updated_at`) VALUES
+(2, 'd', 'd', '1', '2', '', NULL, NULL),
+(3, 'jamu', 'herbal', '90000', '20', '', NULL, NULL),
+(4, 'Buyung upik', 'Kimia', '23456', '10', '', NULL, NULL),
+(5, 'tung tung', 'herball', '78991', '200', '', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `data_obat`
 --
 
 CREATE TABLE `data_obat` (
-  `id` int(10) NOT NULL,
-  `nama_obat` varchar(50) NOT NULL,
-  `jenis_obat` varchar(50) NOT NULL,
-  `harga` int(10) NOT NULL,
-  `pembeli` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama_obat` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jenis_obat` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `harga` char(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `gambar` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data untuk tabel `data_obat`
 --
 
-INSERT INTO `data_obat` (`id`, `nama_obat`, `jenis_obat`, `harga`, `pembeli`) VALUES
-(1, 'Jamu butung Upik', 'Herbal', 25000, 'Burhan'),
-(2, 'Obat Kuat', 'Kimia', 40000, 'Wilcrot'),
-(3, 'Jamu Tolak miskin', 'Jamu', 10000, 'Dobleh'),
-(4, 'ya', 'ya', 20000, 'Jamal');
+INSERT INTO `data_obat` (`id`, `nama_obat`, `jenis_obat`, `harga`, `gambar`, `created_at`, `updated_at`) VALUES
+(1, 'jamu', 'herbal', '200000', 'hijau.jpg', NULL, NULL),
+(2, 'pegel linu', 'kimia', '10000', 'download.jpg', NULL, NULL),
+(5, 'enak', 'diajak ngobrol', '5000', 'download.jpg', NULL, NULL),
+(6, 'satu', 'dua', '9000', '9.PNG', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2020_02_09_094056_create_pasien_table', 1),
+(6, '2020_03_12_081558_data_obat', 2),
+(7, '2020_03_15_115644_data_gudang', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pasien`
+--
+
+CREATE TABLE `pasien` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama_pasein` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ruang` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jenis_kelamin` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `umur` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Pablo', 'hipablo@gmail.com', NULL, '$2y$10$StYJvV9n9yemjhL0XAXCR.SYIYZH5ewjsBtRAYKv7iTRWRPJ2aYWu', NULL, '2020-03-10 00:45:24', '2020-03-10 00:45:24');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `data_gudang`
+--
+ALTER TABLE `data_gudang`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `data_obat`
@@ -57,14 +183,75 @@ ALTER TABLE `data_obat`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indeks untuk tabel `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `pasien`
+--
+ALTER TABLE `pasien`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
+
+--
+-- AUTO_INCREMENT untuk tabel `data_gudang`
+--
+ALTER TABLE `data_gudang`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `data_obat`
 --
 ALTER TABLE `data_obat`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT untuk tabel `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT untuk tabel `pasien`
+--
+ALTER TABLE `pasien`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
